722c8a8fbbd2833b131c419bea1788fed1d9c11a
lalitS <lgsangore2002@gmail.com>
2024-11-12 15:20:06 +0530

    check equality of two lines

--- Line_comparision.py
+++ Line_comparision.py
@@ -13,8 +13,18 @@ class Line:
         length = math.sqrt((self.p2.x - self.p1.x) ** 2 + (self.p2.y - self.p1.y) ** 2)
         return length
     
+    def __eq__(self, other):
+        if isinstance(other, Line):
+            return (self.p1 == other.p1 and self.p2 == other.p2) or \
+                   (self.p1 == other.p2 and self.p2 == other.p1)
+        return False
+    
+
 p1 = Point(1, 2) 
 p2 = Point(4, 6) 
+p3 = Point(1, 2)
+p4 = Point(4, 6)
 
-line = Line(p1, p2)
-print("Length of the line:", line.calculate_length())
+line1 = Line(p1, p2)
+line2 = Line(p3, p4)
+print("lines are equal",line1==line2)
